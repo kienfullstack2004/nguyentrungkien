03975b01bae1ccde393c068fcffcd9e8c2a22296
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2025-05-10 10:41:37 +0700

    avatar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { ProviderStore } from "../../public/config/ProviderStore";
 import "./globals.css";
 
@@ -12,6 +12,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <title>Nguyễn Trung Kiên</title>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
           <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
         </head>
         <body>

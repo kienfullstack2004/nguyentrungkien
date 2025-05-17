48fc7983a42641b57b147a2d7c4d0ae51a7b1fdf
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2025-05-17 14:39:04 +0700

    123456

--- public/config/axiosConfig.tsx
+++ public/config/axiosConfig.tsx
@@ -18,12 +18,8 @@ instance.interceptors.request.use(function (config) {
 
 // Thêm một bộ đón chặn response
 instance.interceptors.response.use(function (response) {
-    // Bất kì mã trạng thái nào nằm trong tầm 2xx đều khiến hàm này được trigger
-    // Làm gì đó với dữ liệu response
     return response;
   }, function (error) {
-    // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
-    // Làm gì đó với lỗi response
     return Promise.reject(error);
   });
 
